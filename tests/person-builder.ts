a2ab65f93418ec54b3484b639bba023549fc4d24
Ronald Veth <r.veth87@outlook.com>
2025-10-29 15:56:22 +0100

    refactor: update formatting in configuration and builder files

--- tests/person-builder.ts
+++ tests/person-builder.ts
@@ -1,33 +1,33 @@
 import { CeriosBuilder } from "../src/cerios-builder";
 
 export type Person = {
-    name: string;
-    age: number;
-    email?: string;
-}
+	name: string;
+	age: number;
+	email?: string;
+};
 
 export class PersonBuilder extends CeriosBuilder<Person> {
-    private constructor(data: Partial<Person>) {
-        super(data);
-    }
+	private constructor(data: Partial<Person>) {
+		super(data);
+	}
 
-    static create() {
-        return new PersonBuilder({});
-    }
+	static create() {
+		return new PersonBuilder({});
+	}
 
-    name(value: string) {
-        return this.setProperty('name', value);
-    }
+	name(value: string) {
+		return this.setProperty("name", value);
+	}
 
-    age(value: number) {
-        return this.setProperty('age', value);
-    }
+	age(value: number) {
+		return this.setProperty("age", value);
+	}
 
-    email(value: string) {
-        return this.setProperty('email', value);
-    }
+	email(value: string) {
+		return this.setProperty("email", value);
+	}
 
-    withAdultAge() {
-        return this.setProperty('age', 18);
-    }
+	withAdultAge() {
+		return this.setProperty("age", 18);
+	}
 }
